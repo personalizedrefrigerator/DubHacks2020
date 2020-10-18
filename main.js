6521ff5e23021a1d4090dc480a8269c4558e2e97
Henry Heino <henryuheino@gmail.com>
2020-10-17 22:24:43 -0700

    Add favicon & resources

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // See https://www.electronjs.org/docs/tutorial/quick-start#create-a-basic-application
 
@@ -9,9 +10,11 @@ function createWindow()
         width: 800,
         height: 600,
         backgroundColor: "#ffffff",
+        titleBarStyle: "hidden",
         webPreferences:
         {
             nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js"), // See https://github.com/AlexTorresSk/custom-electron-titlebar/blob/master/example/main.js
 
             // https://stackoverflow.com/questions/63427191/security-warning-in-the-console-of-browserwindow-electron-9-2-0
             // https://www.electronjs.org/docs/api/browser-window#class-browserwindow
@@ -30,7 +33,7 @@ app.on('window-all-closed', () =>
 {
     if (process.platform !== 'darwin')
     {
-        app.quit(); // MacOS closes the app for us. What about the BSDs?
+        app.quit(); // MacOS -- usually waits to quit until user presses cmd+Q
     }
 });
 
