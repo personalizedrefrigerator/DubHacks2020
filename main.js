8820552a75c9b2f9d142621cb9c3897e55da1e28
Henry Heino <henryuheino@gmail.com>
2020-10-18 00:06:34 -0700

    Remove default menus

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 const path = require('path');
 
 // See https://www.electronjs.org/docs/tutorial/quick-start#create-a-basic-application
@@ -28,6 +28,21 @@ function createWindow()
     win.loadFile('index.html');
     win.webContents.openDevTools();
 }
+
+// Ref: https://www.tutorialspoint.com/electron/electron_menus.htm
+const MENU_TEMPLATE =
+[
+    {
+        label: "Exit",
+        click()
+        {
+            app.quit();
+        }
+    }
+];
+
+const menu = Menu.buildFromTemplate(MENU_TEMPLATE);
+Menu.setApplicationMenu(menu);
 
 app.whenReady().then(createWindow);
 
